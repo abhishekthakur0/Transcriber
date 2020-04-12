c1a85f6aee7c56d36dc86a79c6b0d683d95ce5f9
Abhishek Thakur <kumarcops@gmail.com>
2020-04-13 02:06:06 +0530

    Initial Commit

---
+++ Transcriber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Transcriber
+//
+//  Created by Abhishek Thakur on 13/04/20.
+//  Copyright © 2020 Abhishek Thakur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
